7d048893c6d7d3faae407b696377d9dad4cee802
lifereze <liferezearnold@gmail.com>
2020-08-02 01:00:17 +0300

    insruct the the instrumentation test to launch the main activity before runnning each test

--- app/src/androidTest/java/com/moringaschool/musicapp/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/musicapp/MainActivityInstrumentationTest.java
@@ -1,4 +1,17 @@
 package com.moringaschool.musicapp;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
 }
