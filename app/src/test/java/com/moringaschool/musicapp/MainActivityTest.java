4de62237e783d7ddaafa9ab65d1273bdc15b037a
lifereze <liferezearnold@gmail.com>
2020-08-02 00:52:51 +0300

    create a test to make sure that the correct classs is being tested

--- app/src/test/java/com/moringaschool/musicapp/MainActivityTest.java
+++ app/src/test/java/com/moringaschool/musicapp/MainActivityTest.java
@@ -1,9 +1,16 @@
 package com.moringaschool.musicapp;
 
+import android.content.Intent;
+import android.widget.TextView;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
@@ -13,5 +20,19 @@ public class MainActivityTest {
     public void setup(){
         activity = Robolectric.setupActivity(MainActivity.class);
     }
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("Music Masters".equals(appNameTextView.getText().toString()));
+    }
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.explorebutton).performClick();
+        Intent expectedIntent = new Intent(activity, MusicActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
 
 }
