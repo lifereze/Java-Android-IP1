345572c20c8d67a26289762b535aa6a312430a6f
lifereze <liferezearnold@gmail.com>
2020-08-02 00:18:17 +0300

    add the test dependencies in build gradle file

--- app/src/main/java/com/moringaschool/musicapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/musicapp/MainActivity.java
@@ -2,30 +2,36 @@ package com.moringaschool.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mexplorebutton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mexplorebutton = (Button) findViewById(R.id.explorebutton);
 
-        mexplorebutton = (Button)findViewById(R.id.explorebutton);
         mexplorebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
-                Toast.makeText(MainActivity.this, "Explore Music!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MusicActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
-
 
 }
--- app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
+++ app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
@@ -1,28 +1,25 @@
 package com.moringaschool.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MusicActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MusicActivity extends AppCompatActivity {
 
     private TextView mLocationTextView;
-    private ListView mListView;
-    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
-            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
-            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
-            "Lardo", "Portland City Grill", "Fat Head's Brewery",
-            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurants);
-
-        mListView = (ListView) findViewById(R.id.listView);
+        setContentView(R.layout.activity_music);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
+        mLocationTextView.setText("Music In your Country" + location);
     }
+
 }
