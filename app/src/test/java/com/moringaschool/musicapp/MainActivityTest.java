71136869d0be2405d028262813491e45244429b6
lifereze <liferezearnold@gmail.com>
2020-08-02 00:32:30 +0300

    define the activity to be tested under the test class

---
+++ app/src/test/java/com/moringaschool/musicapp/MainActivityTest.java
@@ -0,0 +1,17 @@
+package com.moringaschool.musicapp;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup(){
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+}
