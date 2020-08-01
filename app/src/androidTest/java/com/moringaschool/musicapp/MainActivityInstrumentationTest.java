6edb14d86afe68c5bd8320cea27806f8b11cd79b
lifereze <liferezearnold@gmail.com>
2020-08-02 01:03:17 +0300

    add a test to verify that the inputted location name is correct

--- app/src/androidTest/java/com/moringaschool/musicapp/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/musicapp/MainActivityInstrumentationTest.java
@@ -4,7 +4,14 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -13,5 +20,11 @@ public class MainActivityInstrumentationTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Nairobi"))
+                .check(matches(withText("Nairobi")));
+    }
 
 }
