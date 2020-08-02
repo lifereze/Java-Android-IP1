ba2426ce3ffbe00eb453cde14d9f2d6d688b208e
lifereze <liferezearnold@gmail.com>
2020-08-03 02:01:27 +0300

    add gitignore file

--- app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
+++ app/src/main/java/com/moringaschool/musicapp/MusicActivity.java
@@ -1,8 +1,8 @@
 package com.moringaschool.musicapp;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,17 +17,16 @@ import butterknife.ButterKnife;
 
 public class MusicActivity extends AppCompatActivity {
 
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.locationTextView)
+    TextView mLocationTextView;
+    @BindView(R.id.listView)
+    ListView mListView;
 
     private String[] music = new String[] {"Drip Too Hard", "Everyday We Lit",
             "Go Flex", "Goosebumps", "The Grinch", "Gucci On My Way",
             "Money In the Grave", "Bounce Back", "Letter To My Daughter", "My Dawg",
             "Bouff Daddy", "Close Friends", "Congratulations",
             "Blow my Mind", "Dirty Iyanna"};
-    private String[] artist = new String[] {"Lil Baby", "YFN Lucci", "Migos", "Travis Scoot", "Polo G", "Will 23", "Drake", "Big Sean", "Rondo", "Lil Baby", "Domani Mungu", "Lil Baby", "Post Malone", "Davido", "Nasty C"};
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,25 +34,23 @@ public class MusicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music);
         ButterKnife.bind(this);
 
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-        mListView = (ListView) findViewById(R.id.listView);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, music);
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, music);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, music);
         mListView.setAdapter(adapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String music = ((TextView)view).getText().toString();
-
-//                Log.v("MusicActivity", "In the onItemClickListener!");
                 Toast.makeText(MusicActivity.this, music, Toast.LENGTH_LONG).show();
             }
         });
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText("Music In your Location" + "" +location);
+
+
+        mLocationTextView.setText("Music In Your Location " + location);
     }
 
 }
--- app/src/main/java/com/moringaschool/musicapp/MyMusicArrayAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.moringaschool.musicapp;
-
-public class MyMusicArrayAdapter {
-}
