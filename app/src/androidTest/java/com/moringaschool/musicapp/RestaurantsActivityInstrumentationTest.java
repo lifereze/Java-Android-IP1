78785a1cb52e86587ade1347fe50336656a75e40
lifereze <liferezearnold@gmail.com>
2020-08-02 09:32:22 +0300

    initial commit

--- app/src/androidTest/java/com/moringaschool/musicapp/RestaurantsActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/musicapp/RestaurantsActivityInstrumentationTest.java
@@ -1,4 +1,14 @@
 package com.moringaschool.musicapp;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class RestaurantsActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MusicActivity> activityTestRule =
+            new ActivityTestRule<>(MusicActivity.class);
 }
