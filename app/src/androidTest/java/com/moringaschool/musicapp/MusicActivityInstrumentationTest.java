a3ce69d54bd83f2883a8fc17a7e85b0955868b43
lifereze <liferezearnold@gmail.com>
2020-08-02 09:35:33 +0300

    add the test

---
+++ app/src/androidTest/java/com/moringaschool/musicapp/MusicActivityInstrumentationTest.java
@@ -0,0 +1,39 @@
+package com.moringaschool.musicapp;
+
+import android.view.View;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class MusicActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MusicActivity> activityTestRule =
+            new ActivityTestRule<>(MusicActivity.class);
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String musicName = "Mi Mero Mole";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(musicName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(musicName)));
+    }
+}
